342dbb5c3169a9d80e346dff1d41ec2d442c2940
Bakamura <ra00297288@pucsp.edu.br>
2022-09-07 11:13:56 -0300

    Basic Setup

---
+++ src/main/java/com/Nakanzaki/nakaSkyblock/NakaSkyblock.java
@@ -0,0 +1,8 @@
+package com.Nakanzaki.nakaSkyblock;
+
+import net.minecraftforge.fml.common.Mod;
+
+@Mod(NakaSkyblock.MODID)
+public class NakaSkyblock {
+    public static final String MODID = "nakaskyblock";
+}
